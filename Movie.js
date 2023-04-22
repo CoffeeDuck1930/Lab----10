29dc5d52af0ada12ec9b5e5edce64bcea0944a16
CoffeeDuck1930 <127752126+CoffeeDuck1930@users.noreply.github.com>
2023-04-21 23:42:22 -0500

    Add files via upload

---
+++ Movie.js
@@ -0,0 +1,10 @@
+class Movie {
+    constructor (name, rating, description, bannerimg, cardimg, clasification) {
+        this.name = name
+        this.rating = rating
+        this.description = description
+        this.imagen_f = bannerimg
+        this.imagen_L = cardimg
+        this.clasification = clasification
+    }
+}
